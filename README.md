41f3f9620cfc186ff1c7a4bcdee2829aac5de263
Hannah Woodward <hannah@serieseight.com>
2019-06-08 10:44:37 +0100

    Replace tabs with spaces

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ All settings are optional, defaults are listed below:
 
 ```
 new ScrollSentinel( {
-	container: window,	// the container to monitor the scroll position of
-	items: [],		// an array of ScrollSentinel items - see below
-	forceRun: false
+  container: window,  // the container to monitor the scroll position of
+  items: [],    // an array of ScrollSentinel items - see below
+  forceRun: false
 } );
 ```
 
@@ -22,12 +22,12 @@ NB: Item callbacks are only run on vertical scroll change by default. `forceRun`
 When triggered, ScrollSentinel loops through its items and runs the function fx which takes the element as the argument. An item can be pushed to the ScrollSentinel instance either at creation or by using the add() method:
 ```
 const item = {
-	element: SOME_EL, 	// REQUIRED could be a DOM node, array, object, etc,
-	fx: ( el ) => {},	// REQUIRED the callback to run on the element
-	dyThrottle: true 	// DEFAULT: true - set to false if you would like the callback to be run on every frame
+  element: SOME_EL,   // REQUIRED could be a DOM node, array, object, etc,
+  fx: ( el ) => {},  // REQUIRED the callback to run on the element
+  dyThrottle: true   // DEFAULT: true - set to false if you would like the callback to be run on every frame
 };
 const sm = new ScrollSentinel({
-	items: [ item ]
+  items: [ item ]
 });
 
 // Equivalently, you can add a single item:
@@ -45,18 +45,18 @@ const uncloak = new Uncloak();
 const some_node = document.querySelector( '.some-node' );
 
 sm.add( {
-	element: uncloak,
-	fx: ( obj ) => {
-		obj.processItems(); // uncloak function
-	},
-	dyThrottle: false // run on every frame
+  element: uncloak,
+  fx: ( obj ) => {
+    obj.processItems(); // uncloak function
+  },
+  dyThrottle: false // run on every frame
 } );
 
 // The below function will still only run on DY change as dyThrottle is set per item (and defaults to true)
 sm.add( {
-	element: some_node,
-	fx: ( node ) => {
-		console.log( 'DY changed' );
-	}
+  element: some_node,
+  fx: ( node ) => {
+    console.log( 'DY changed' );
+  }
 } );
 ```
--- scroll-sentinel.js
+++ scroll-sentinel.js
@@ -1,94 +1,94 @@
 export default class ScrollSentinel {
-	constructor( options ) {
-		options = options || {};
-		const defaults = {
-			container: window,
-			items: [],
-			forceRun: false
-		};
-		if ( options ) {
-			for ( const key in defaults ) {
-				this[key] = options[key] || defaults[key];
-			}
-		}
+  constructor( options ) {
+    options = options || {};
+    const defaults = {
+      container: window,
+      items: [],
+      forceRun: false
+    };
+    if ( options ) {
+      for ( const key in defaults ) {
+        this[key] = options[key] || defaults[key];
+      }
+    }
 
-		// initial load
-		this.init();
-	}
+    // initial load
+    this.init();
+  }
 
-	init() {
-		let is_ready = true;
-		const runItemCallbacks = ( dy_changed = true ) => {
-			for ( let i = 0; i < this.items.length; i++ ) {
-				const item = this.items[i];
-				if ( dy_changed || item.dxReliant ) {
-					item.fx( item.element );
-				}
-			}
-			return true;
-		};
+  init() {
+    let is_ready = true;
+    const runItemCallbacks = ( dy_changed = true ) => {
+      for ( let i = 0; i < this.items.length; i++ ) {
+        const item = this.items[i];
+        if ( dy_changed || item.dxReliant ) {
+          item.fx( item.element );
+        }
+      }
+      return true;
+    };
 
-		const fps = 60;
-		const _this = this;
-		let y0 = getScrollY();
-		let t0 = getTime();
+    const fps = 60;
+    const _this = this;
+    let y0 = getScrollY();
+    let t0 = getTime();
 
-		requestAnimationFrame( update );
-		if ( this.container === window ) {
-			window.addEventListener( 'resize', runItemCallbacks );
-		}
+    requestAnimationFrame( update );
+    if ( this.container === window ) {
+      window.addEventListener( 'resize', runItemCallbacks );
+    }
 
-		function update() {
-			requestAnimationFrame( update );
-			const t1 = getTime();
-			if ( !is_ready || ( t1 - t0 ) < fps ) {
-				return;
-			}
+    function update() {
+      requestAnimationFrame( update );
+      const t1 = getTime();
+      if ( !is_ready || ( t1 - t0 ) < fps ) {
+        return;
+      }
 
-			const y1 = getScrollY();
-			const y_changed = ( y1 !== y0 );
-			// return early if no elements have DY throttling disabled
-			if ( !y_changed && !_this.forceRun ) {
-				return;
-			}
-			is_ready = false;
+      const y1 = getScrollY();
+      const y_changed = ( y1 !== y0 );
+      // return early if no elements have DY throttling disabled
+      if ( !y_changed && !_this.forceRun ) {
+        return;
+      }
+      is_ready = false;
 
-			y0 = y1;
-			t0 = t1;
-			is_ready = runItemCallbacks( y_changed );
-		}
-		function getScrollY() {
-			if ( _this.container === window ) {
-				return ( window.scrollY || document.documentElement.scrollTop );
-			}
-			return _this.container.scrollTop;
-		}
-		function getTime() {
-			return performance.now();
-		}
-	}
+      y0 = y1;
+      t0 = t1;
+      is_ready = runItemCallbacks( y_changed );
+    }
+    function getScrollY() {
+      if ( _this.container === window ) {
+        return ( window.scrollY || document.documentElement.scrollTop );
+      }
+      return _this.container.scrollTop;
+    }
+    function getTime() {
+      return performance.now();
+    }
+  }
 
-	add( item ) {
-		// FORMAT: { element: el, dyThrottle: false, fx: () => {} }
-		if ( !item.element || !item.fx ) {
-			return;
-		}
-		item.dyThrottle = item.dyThrottle || true;
-		this.items.push( item );
-		if ( !item.dyThrottle ) {
-			this.forceRun = true;
-		}
-	}
+  add( item ) {
+    // FORMAT: { element: el, dyThrottle: false, fx: () => {} }
+    if ( !item.element || !item.fx ) {
+      return;
+    }
+    item.dyThrottle = item.dyThrottle || true;
+    this.items.push( item );
+    if ( !item.dyThrottle ) {
+      this.forceRun = true;
+    }
+  }
 
-	clearItems() {
-		this.items = [];
-	}
+  clearItems() {
+    this.items = [];
+  }
 
-	// stop() {
-	// 	this.clearItems();
-	// 	cancelAnimationFrame( update );
-	// 	if ( this.container === window ) {
-	// 		window.removeEventListener( 'resize', runItemCallbacks );
-	// 	}
-	// }
+  // stop() {
+  //   this.clearItems();
+  //   cancelAnimationFrame( update );
+  //   if ( this.container === window ) {
+  //     window.removeEventListener( 'resize', runItemCallbacks );
+  //   }
+  // }
 }
