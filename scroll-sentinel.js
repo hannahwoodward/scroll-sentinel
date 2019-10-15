f5236daa58b3e2e4d4eeb61f87aa516de76d453d
Hannah Woodward <hannah@serieseight.com>
2019-10-15 22:07:39 +0100

    Update dyThrottle to general dsThrottle (covers both x and y axis)

--- scroll-sentinel.js
+++ scroll-sentinel.js
@@ -18,10 +18,10 @@ export default class ScrollSentinel {
 
   init() {
     let is_ready = true
-    const runItemCallbacks = (dy_changed = true) => {
+    const runItemCallbacks = (ds_changed = true) => {
       for (let i = 0; i < this.items.length; i++) {
         const item = this.items[i]
-        if (dy_changed || item.dxReliant) {
+        if (ds_changed || !item.dsThrottle) {
           item.fx(item.element)
         }
       }
@@ -30,6 +30,7 @@ export default class ScrollSentinel {
 
     const fps = 60
     const _this = this
+    let x0 = getScrollX()
     let y0 = getScrollY()
     let t0 = getTime()
 
@@ -45,17 +46,26 @@ export default class ScrollSentinel {
         return
       }
 
+      const x1 = getScrollX()
       const y1 = getScrollY()
+      const x_changed = (x1 !== x0)
       const y_changed = (y1 !== y0)
       // return early if no elements have DY throttling disabled
-      if (!y_changed && !_this.forceRun) {
+      if (!x_changed && !y_changed && !_this.forceRun) {
         return
       }
       is_ready = false
 
+      x0 = x1
       y0 = y1
       t0 = t1
-      is_ready = runItemCallbacks(y_changed)
+      is_ready = runItemCallbacks(x_changed || y_changed)
+    }
+    function getScrollX() {
+      if (_this.container === window) {
+        return (window.scrollX || document.documentElement.scrollLeft)
+      }
+      return _this.container.scrollLeft
     }
     function getScrollY() {
       if (_this.container === window) {
@@ -69,13 +79,13 @@ export default class ScrollSentinel {
   }
 
   add(item) {
-    // FORMAT: { element: el, dyThrottle: false, fx: () => {} }
+    // FORMAT: { element: el, dsThrottle: false, fx: () => {} }
     if (!item.element || !item.fx) {
       return
     }
-    item.dyThrottle = item.dyThrottle || true
+    item.dsThrottle = 'dsThrottle' in item ? item.dsThrottle : true
     this.items.push(item)
-    if (!item.dyThrottle) {
+    if (!item.dsThrottle) {
       this.forceRun = true
     }
   }
